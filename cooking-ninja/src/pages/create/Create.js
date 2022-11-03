9d09e4b24b898a307be838274913034ac819b8ce
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-11-03 15:40:36 +0600

    Building the create page

--- cooking-ninja/src/pages/create/Create.js
+++ cooking-ninja/src/pages/create/Create.js
@@ -1,9 +1,52 @@
 import './Create.css'
 
-import React from 'react'
+import { useState } from 'react';
 
-export default function create() {
+export default function Create() {
+  const [title, setTitle] = useState('');
+  const [method, setMethod] = useState('');
+  const [cookingTime, setCookingTime] = useState('');
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    console.log(title, method, cookingTime);
+  }
   return (
-    <div>create</div>
+    <div className='create'>
+      <h2 className='page-title'>Add a new Recipe</h2>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Recipe Title: </span>
+        <input
+          type='text'
+          onChange={(e)=> setTitle(e.target.value)}
+          value={title}
+          required
+          />
+       </label>
+
+        <label>
+          <span>Recipe method: </span>
+          <textarea
+            onChange = {(e) => setMethod(e.target.value)}
+            value = {method}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Cooking time: </span>
+          <input
+            type="number"
+            onChange = {(e)=> setCookingTime(e.target.value)}
+            value = {cookingTime}
+            required
+          />
+        </label>
+        <button className='btn'>Submit</button>
+        
+      </form>
+    </div>
   )
 }
