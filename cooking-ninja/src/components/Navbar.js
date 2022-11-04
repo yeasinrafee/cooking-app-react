f541673b07d716a5ccf3af4bd0364f61c6980dcc
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-11-05 00:06:49 +0600

    Searchbar component added

--- cooking-ninja/src/components/Navbar.js
+++ cooking-ninja/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 
 import './Navbar.css';
+import Searchbar from './Searchbar';
 
 export default function Navbar() {
   return (
@@ -9,6 +10,7 @@ export default function Navbar() {
             <Link to="/" className='brand'>
                 <h1>Cooking Bro</h1>
             </Link>
+            <Searchbar/>
             <Link to="/create">Create Recipe</Link>
         </nav>
     </div>
--- cooking-ninja/src/components/Searchbar.js
+++ cooking-ninja/src/components/Searchbar.js
@@ -1,9 +1,28 @@
 import './Searchbar.css';
 
 import React from 'react'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Searchbar() {
+    const [term, setTerm] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        navigate(`/search?q=${term}`);
+    }
   return (
-    <div>Searchbar</div>
+    <div className="searchbar">
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="search">Search: </label>
+            <input 
+                type="text"
+                id="search"
+                onChange={(e) =>setTerm(e.target.value)}
+                required
+             />
+        </form>
+    </div>
   )
 }
